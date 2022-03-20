cb5aad799aeb3c1a4b4e41094c2a0fe5f8de26f3
hisasu <perusyanekolove0320@gmail.com>
2022-03-20 10:47:48 +0900

    add rnn.ipynb in the repo and some functionsin in .py files

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
 
@@ -17,3 +18,26 @@ class MLP(nn.Module):
         x = self.dropout(F.relu(self.fc2(x)))
         x = self.fc3(x)
         return x
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout_ratio):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, 8)
+        self.fc2 = nn.Linear(8, 5)
+        self.fc3 = nn.Linear(5, num_classes)
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, x, device):
+        h0 = torch.zeros(self.num_layers, x.shape[0], self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.shape[0], self.hidden_size).to(device)
+
+        out, _ = self.rnn(x, (h0,c0))
+        out = self.dropout(F.relu(self.fc1(out)))
+        out = self.dropout(F.relu(self.fc2(out)))
+        out = F.relu(self.fc3(out))
+        return out
--- train_model.py
+++ train_model.py
@@ -46,3 +46,48 @@ def train_net(model, criterion, optimizer, num_epochs, train_loader, test_x, tes
     torch.save(model.state_dict(), "./mlp.pth")
 
     return model
+
+def train_timeseries_net(model, criterion, optimizer, num_epochs,
+                         train_loader, test_x, test_y, device):
+    train_losses = []
+    test_losses = []
+
+    for _ in range(num_epochs):
+        runnning_loss = 0.0
+        for idx, (batch_x, batch_y) in enumerate(train_loader):
+            batch_x = batch_x.to(device)
+            batch_y = batch_y.to(device)
+
+            # forward
+            score_y = model(batch_x, device)
+            loss = criterion(score_y.reshape(-1, 2), batch_y.reshape(-1))
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # update params
+            torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=1)
+            optimizer.step()
+            #torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=1)
+
+            # add training loss
+            runnning_loss += loss.item()
+        train_losses.append(runnning_loss / idx)
+
+        # add test loss
+        pred_y = model(test_x, device)
+        test_loss = criterion(pred_y.reshape(-1, 2), test_y.reshape(-1))
+        test_losses.append(test_loss.item())
+
+    # plot loss curve
+    plt.plot(train_losses, label="train")
+    plt.plot(test_losses, label="test")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.legend()
+    plt.savefig("loss.png")
+
+    # save state_dict
+    torch.save(model.state_dict(), "./rnn.pth")
+    return model
