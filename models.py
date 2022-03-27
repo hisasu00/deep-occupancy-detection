83064d4fb4c7cfc78474626fd6ea491f9574571b
hisasu <perusyanekolove0320@gmail.com>
2022-03-27 12:28:17 +0900

    add seq2seq.ipynb and some functions needed for runnning .ipynb

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import random
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -66,7 +67,7 @@ class AttentionRNN(nn.Module):
         c0 = torch.zeros(self.num_layers, x.shape[0], self.hidden_size).to(device)
         hs, _ = self.rnn(x, (h0,c0))
 
-        contexts = get_contexts_by_attention(hs, device)
+        contexts = get_contexts_by_selfattention(hs, device)
         out = torch.cat((contexts, hs), dim=2)
 
         out = self.dropout1(F.relu(self.fc1(out)))
@@ -94,3 +95,99 @@ def get_contexts_by_selfattention(hs, device):
         context = (attention*hs).sum(axis=1)
         contexts[:, t, :] = context
     return contexts
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout_ratio):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size,
+                            num_layers, batch_first=True, dropout=dropout_ratio)
+        self.bn1 = nn.BatchNorm1d(hidden_size)
+
+    def forward(self, x):
+        encoder_hs, (h_t, c_t)  = self.lstm(x)
+        encoder_hs = encoder_hs.reshape(x.shape[0], encoder_hs.shape[2], x.shape[1])
+        encoder_hs = self.bn1(encoder_hs)
+        encoder_hs = encoder_hs.reshape(x.shape[0], x.shape[1], encoder_hs.shape[1])
+        return encoder_hs, h_t, c_t
+
+
+class Decoder(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers, dropout_ratio):
+        super().__init__()
+        self.rnn = nn.LSTM(input_size, hidden_size,
+                           num_layers, batch_first=True, dropout=dropout_ratio)
+        self.bn1 = nn.BatchNorm1d(2 * hidden_size)
+        self.fc = nn.Linear(2 * hidden_size, output_size)
+
+    def forward(self, x, h_t, c_t, encoder_hs, device):
+        decoder_hs, (h_t, c_t) = self.rnn(x, (h_t, c_t))
+        outputs = get_contexts_and_decoderhs_by_attention(encoder_hs, decoder_hs, device)
+
+        outputs = outputs.reshape(x.shape[0], outputs.shape[2], x.shape[1])
+        outputs = self.bn1(outputs)
+        outputs = outputs.reshape(x.shape[0], x.shape[1], outputs.shape[1])
+
+        predictions = self.fc(outputs)
+        return predictions
+
+    def generate(self, h_t, c_t, encoder_hs, x, target_len, device):
+        start_x = x[:, 0]
+        sampled = torch.zeros(start_x.shape[0], target_len).to(device)
+        scores = torch.zeros(start_x.shape[0], target_len, 2).to(device)
+
+        for t in range(target_len):
+            start_x = start_x.unsqueeze(2)
+            start_x = start_x.to(torch.float32)
+
+            decoder_hs, (h_t, c_t) =  self.rnn(start_x, (h_t, c_t))
+            output = get_contexts_and_decoderhs_by_attention(encoder_hs, decoder_hs, device)
+
+            output = output.reshape(start_x.shape[0], output.shape[2], start_x.shape[1])
+            output = self.bn1(output)
+            output = output.reshape(start_x.shape[0], start_x.shape[1], output.shape[1])
+            output = self.fc(output)
+            predict = output.argmax(axis=2)
+
+            start_x = x[:, t] if random.random() < 0.5 else predict
+            sampled[:, t] = start_x.reshape(-1)
+            output = output.squeeze(1)
+            scores[:, t, :] = output
+        return sampled, scores
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, enc_input, dec_input, device):
+        encoder_hs, h_t, c_t = self.encoder(enc_input)
+        outputs = self.decoder(dec_input, h_t, c_t, encoder_hs, device)
+        return outputs
+
+    def generate(self, enc_input, dec_input, target_len, device):
+        encoder_hs, h_t, c_t = self.encoder.forward(enc_input)
+        sampled, scores = self.decoder.generate(h_t, c_t, encoder_hs, dec_input, target_len, device)
+        return sampled, scores
+
+
+def get_contexts_and_decoderhs_by_attention(encoder_hs, decoder_hs, device):
+    N, decoder_T, H = decoder_hs.shape
+    encoder_T = encoder_hs.shape[1]
+    outputs = torch.zeros(N, decoder_T, 2 * H).to(device)
+
+    for t in range(decoder_T):
+        decoder_ht = decoder_hs[:, t, :].unsqueeze(1)
+        decoder_ht = decoder_ht.repeat(1, encoder_T, 1)
+
+        attention_weight = (encoder_hs*decoder_ht).sum(axis=2)
+        attention_weight = F.softmax(attention_weight, dim=1)
+        attention_weight = attention_weight.unsqueeze(2)
+        attention_weight = attention_weight.repeat(1, 1, H)
+
+        context_vector = (attention_weight*encoder_hs).sum(axis=1)
+
+        outputs[:, t, :] = torch.cat((context_vector, decoder_hs[:, t, :]), dim=1)
+    return outputs
--- train_model.py
+++ train_model.py
@@ -91,3 +91,44 @@ def train_timeseries_net(model, criterion, optimizer, num_epochs,
     # save state_dict
     torch.save(model.state_dict(), "./rnn.pth")
     return model
+
+
+def train_seq2seq_net(model, criterion, optimizer,
+                      num_epochs, train_loader, device):
+    train_losses = []
+
+    for _ in range(num_epochs):
+        runnning_loss = 0.0
+        for idx, (batch_x, batch_y) in enumerate(train_loader):
+            batch_x = batch_x.to(device)
+            batch_y = batch_y.to(device)
+            decoder_yy = batch_y[:, :-1, :]
+            decoder_tt = batch_y[:, 1:, :].to(torch.long)
+
+            # forward
+            score_y = model(batch_x, decoder_yy, device)
+            loss = criterion(score_y.reshape(-1, 2), decoder_tt.reshape(-1))
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # update params
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
+
+            # add training loss
+            runnning_loss += loss.item()
+        train_losses.append(runnning_loss / idx)
+
+    # plot loss curve
+    plt.plot(train_losses, label="train", alpha=0.5, c="r")
+    plt.ylim(0.2, 1.0)
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.legend()
+    plt.savefig("loss.png")
+
+    # save state_dict
+    torch.save(model.state_dict(), "./seq2seq.pth")
+    return model
