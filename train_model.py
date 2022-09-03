78e3226b9ea004486f30300a4018295758b74a69
hisasu <perusyanekolove0320@gmail.com>
2022-09-03 11:07:08 +0900

    delete unused variable self.val_loss_min = np.Inf

--- train_model.py
+++ train_model.py
@@ -190,7 +190,6 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
         self.delta = delta
 
     def __call__(self, val_loss):
